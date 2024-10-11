b3540abaa4d64c344ed8d5c60dd276e4f510004a
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-10-11 16:21:53 +0530

    remove unnecessary code

--- app/utils/contentstack.server.ts
+++ app/utils/contentstack.server.ts
@@ -1,6 +1,3 @@
-import { GraphQLClient } from "graphql-request"
 
-export const contentstackGraphQLClient = new GraphQLClient(
-    process.env.CONTENTSTACK_API_KEY as string
-);
+// TODO: add methods to fetch entries from contentstack
 
