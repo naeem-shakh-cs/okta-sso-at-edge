d8d7f8ef85c384ea3ea92f6cd0d38e03410fa315
Chhavi-Mandowara <chhavi.mandowara@contentstack.com>
2024-10-16 14:54:26 +0530

    Add theme text color to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="py-5 border-t light:border-gray-900/10 dark:border-[#333]">
       <div className="font-montserrat m-auto max-w-md block py-1 px-3 rounded-full text-center built-by" data-cslp="menu.blt787be50a3867cfeb.en.built_by">
-        <p>
+        <p className="text-theme-text">
           Built with <a href="https://www.contentstack.com/headless-cms" target="_blank" className="text-contentstack font-semibold ">Contentstack</a>
           . Hosted on <a href="https://www.contentstack.com/launch" target="_blank" className="text-contentstack font-semibold ">Launch</a>.
         </p>
