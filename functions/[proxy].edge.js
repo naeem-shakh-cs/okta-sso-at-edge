c9599d696ed4eaacb79a380755c29adff82d25bf
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-11-01 23:34:28 +0530

    fix

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -46,9 +46,10 @@ async function verifySAMLResponseSignature(samlResponse, x509Cert, spEntityId) {
   
     const jsonResponse = JSON.parse(xml2json(decodedResponse, { compact: true }));
     console.log(jsonResponse)
-    const signatureValue = jsonResponse['saml2p:Response']['ds:Signature']['ds:SignatureValue']._text;
-    const signedInfo = jsonResponse['saml2p:Response']['ds:Signature']['ds:SignedInfo'];
     const assertion = jsonResponse['saml2p:Response']['saml2:Assertion'];
+    const signatureValue = assertion['ds:Signature']['ds:SignatureValue']._text;
+    const signedInfo = assertion['ds:Signature']['ds:SignedInfo'];
+    
   
     const validEntityId = verifyEntityId(assertion, spEntityId)
     if(!validEntityId) {
